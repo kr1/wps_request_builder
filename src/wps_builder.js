30157d520b38126629393df3abebb356d238b7d7
Christian Wörner <christianworner@gmail.com>
2013-11-18 00:48:38 +0100

    refactor main builder script to accomodate chained processes

--- src/wps_builder.js
+++ src/wps_builder.js
@@ -1,101 +1,124 @@
 (function (exports) {
 
-    exports.makeExecuteDocument = function () {
-        exports.docu = new DOMParser().parseFromString("" +
-                  "<wps:Execute version='1.0.0' service='WPS' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " +
-                  "xmlns='http://www.opengis.net/wps/1.0.0' xmlns:wfs='http://www.opengis.net/wfs' " +
-                  "xmlns:wps='http://www.opengis.net/wps/1.0.0' xmlns:ows='http://www.opengis.net/ows/1.1' " +
-                  "xmlns:gml='http://www.opengis.net/gml' xmlns:ogc='http://www.opengis.net/ogc' " +
-                  "xmlns:wcs='http://www.opengis.net/wcs/1.1.1' xmlns:xlink='http://www.w3.org/1999/xlink' " +
-                  "xsi:schemaLocation='http://www.opengis.net/wps/1.0.0 " +
-                  "http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd'></wps:Execute>",
-                  "application/xml");
-        exports.exec = exports.docu.firstChild;
-        return exports.docu;
+    exports.makeExecuteDocument = function (options) {
+        var namespaces = "xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " +
+            " xmlns='http://www.opengis.net/wps/1.0.0'  xmlns:wfs='http://www.opengis.net/wfs' " +
+            "xmlns:ows='http://www.opengis.net/ows/1.1' " +
+            "xmlns:gml='http://www.opengis.net/gml' xmlns:ogc='http://www.opengis.net/ogc' " +
+            "xmlns:wcs='http://www.opengis.net/wcs/1.1.1' xmlns:xlink='http://www.w3.org/1999/xlink' " +
+            "xsi:schemaLocation='http://www.opengis.net/wps/1.0.0 " +
+            "http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd'",
+            docu;
+        if (options && options.withoutNamespaces) {
+            namespaces = '';
+        }
+        docu = new DOMParser().parseFromString("" +
+            "<wps:Execute version='1.0.0' service='WPS' xmlns:wps='http://www.opengis.net/wps/1.0.0'" +
+            namespaces + "></wps:Execute>", "application/xml");
+        return docu;
     };
 
-    exports.addIdentifier = function (ident) {
-        var ideP = exports.docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Identifier");
-        var tide = exports.docu.createTextNode(ident);
+    exports.addIdentifier = function (ident, docu, exec) {
+        var ideP = docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Identifier");
+        var tide = docu.createTextNode(ident);
         ideP.appendChild(tide);
-        exports.exec.appendChild(ideP);
+        exec.appendChild(ideP);
     };
 
-    exports.addInputs = function (inputsDef) {
-        var inputs = exports.docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:DataInputs"),
+    exports.addInputs = function (inputsDef, docu, exec) {
+        var inputs = docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:DataInputs"),
             input;
         for (var idx = 0; idx < inputsDef.length; idx++) {
-            input = exports.addInput(inputsDef[idx]);
+            input = exports.addInput(inputsDef[idx], docu, exec);
             inputs.appendChild(input);
         }
-        exports.exec.appendChild(inputs);
+        exec.appendChild(inputs);
     };
 
-    exports.addInput = function (inputDef) {
-        var input = exports.docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Input"),
-            identifier = exports.docu.createElement("ows:Identifier"),
-            identifierCont = exports.docu.createTextNode(inputDef.identifier),
-            title = exports.docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Title"),
-            titleCont = exports.docu.createTextNode(inputDef.identifier),
+    exports.addInput = function (inputDef, docu) {
+        var input = docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Input"),
+            identifier = docu.createElement("ows:Identifier"),
+            identifierCont = docu.createTextNode(inputDef.identifier),
+            title = docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Title"),
+            titleCont = docu.createTextNode(inputDef.identifier),
             data, cd, cdCont, reference, body, getFeature, query;
         if (inputDef.mimeType && inputDef.mimeType === "application/wkt") {
-            data = exports.docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Data");
-            cd = exports.docu.createElement("wps:ComplexData");
+            data = docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Data");
+            cd = docu.createElement("wps:ComplexData");
             cd.setAttribute("mimeType", inputDef.mimeType);
-            cdCont = exports.docu.createCDATASection(inputDef.data);
+            cdCont = docu.createCDATASection(inputDef.data);
             cd.appendChild(cdCont);
             data.appendChild(cd);
             input.appendChild(data);
         } else if (inputDef.reference) {
             if (inputDef.reference.type === "wfs") {
-                reference = exports.docu.createElement("wps:Reference");
+                reference = docu.createElement("wps:Reference");
                 reference.setAttribute("mimeType", "text/xml");
                 reference.setAttribute("xlink:href", "http://geoserver/wfs");
                 reference.setAttribute("method", "POST");
-                body = exports.docu.createElement("wps:Body");
-                getFeature = exports.docu.createElement("wfs:GetFeature");
+                body = docu.createElement("wps:Body");
+                getFeature = docu.createElement("wfs:GetFeature");
                 getFeature.setAttribute("service", "WFS");
                 getFeature.setAttribute("version", "1.0.0");
                 getFeature.setAttribute("outputFormat", "GML2");
                 getFeature.setAttribute("xmlns:" + inputDef.typeName.split(":")[0],
                                         (inputDef.typeNamespace || "http://www.openplans.org/topp"));
-                query = exports.docu.createElement("wfs:Query");
+                query = docu.createElement("wfs:Query");
                 query.setAttribute("typeName", inputDef.typeName);
                 reference.appendChild(body);
                 body.appendChild(getFeature);
                 getFeature.appendChild(query);
                 input.appendChild(reference);
             }
+        } else if (inputDef.subprocess) {
+            var proc = inputDef.subprocess,
+                subtreeDoc = exports.makeExecuteDocument({withoutNamespaces: true}),
+                subtree = subtreeDoc.firstChild,
+                reference = subtreeDoc.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Reference"),
+                body = subtreeDoc.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:Body");
+            reference.setAttribute("mimeType", "text/xml; subtype=gml/3.1.1");
+            reference.setAttribute("xlink:href", "http://geoserver/wps");
+            reference.setAttribute("method", "POST");
+            reference.appendChild(body);
+            body.appendChild(subtree);
+            exports.addIdentifier(proc.identifier, subtreeDoc, body);
+            // Inputs
+            exports.addInputs(proc.inputs, subtreeDoc, body);
+            // ResposeForm
+            exports.addResponseForm(proc.response, subtreeDoc, body);
+            input.appendChild(reference);
         }
         identifier.appendChild(identifierCont);
-        title.appendChild(titleCont);
-        input.appendChild(title);
+        // TODO: check: is title necessary?
+        //title.appendChild(titleCont);
+        //input.appendChild(title);
         input.appendChild(identifier);
         return input;
     };
 
-    exports.addResponseForm = function (responseDef) {
-        var resp = exports.docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:ResponseForm"),
-            rawDataOut = exports.docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:RawDataOutput");
+    exports.addResponseForm = function (responseDef, docu, exec) {
+        var resp = docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:ResponseForm"),
+            rawDataOut = docu.createElementNS("http://www.opengis.net/wps/1.0.0", "wps:RawDataOutput");
         rawDataOut.setAttribute("mimeType", responseDef.mimeType);
-        var ident = exports.docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Identifier"),
-            identCont = exports.docu.createTextNode(responseDef.identifier);
+        var ident = docu.createElementNS("http://www.opengis.net/ows/1.1", "ows:Identifier"),
+            identCont = docu.createTextNode(responseDef.identifier);
         ident.appendChild(identCont);
         rawDataOut.appendChild(ident);
         resp.appendChild(rawDataOut);
-        exports.exec.appendChild(resp);
+        exec.appendChild(resp);
     };
 
     exports.build = function (configuration) {
         // Process
-        var docu = exports.makeExecuteDocument();
+        exports.docu = exports.makeExecuteDocument();
+        exports.exec = exports.docu.firstChild;
             //exec = docu.getElementsByTagName("Execute")[0];
-        exports.addIdentifier(configuration.identifier);
+        exports.addIdentifier(configuration.identifier, exports.docu, exports.exec);
         // Inputs
-        exports.addInputs(configuration.inputs);
+        exports.addInputs(configuration.inputs, exports.docu, exports.exec);
         // ResposeForm
-        exports.addResponseForm(configuration.response);
-        return docu;
+        exports.addResponseForm(configuration.response, exports.docu, exports.exec);
+        return exports.docu;
     };
 
     exports.serialize = function (xml) {

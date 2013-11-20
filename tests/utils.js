c98e7929dfdc008b43400779b377369652b2dd4b
Christian Wörner <christianworner@gmail.com>
2013-11-20 22:33:56 +0100

    adds a function to walk the dom

---
+++ tests/utils.js
@@ -0,0 +1,13 @@
+// Define a walk_the_DOM function that visits every
+// node of the tree in HTML source order, starting
+// from some given node. It invokes a function,
+// passing it each node in turn. walk_the_DOM calls
+// itself to process each of the child nodes.
+var walk_the_DOM = function walk(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walk(node, func);
+        node = node.nextSibling;
+    }
+};
